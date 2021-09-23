f42ebc6b3fcda7409b04d0025f727db77c65a191
SampleUser0001 <ittimfn+github@gmail.com>
2021-09-23 18:11:46 +0900

    日付型, int型の扱い

--- app/src/dataclass/sample_data.py
+++ app/src/dataclass/sample_data.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 from dataclasses import dataclass, field
+import datetime
 from typing import List
 
 @dataclass
 class Piyo01:
   piyo11: str
   piyo12: str
-  notDefinedInJson: str = None
-  
+
 @dataclass
 class Fuga01:
   fuga11: str
   fuga12: str
-  notDefinedInJson: str = None
 
 @dataclass
 class SampleJsonData:
-  hoge01: str = ''
+  hoge01: str = None
   piyo01: Piyo01 = None
   fuga01: List[Fuga01] = field(default_factory=list)
   notDefinedInJson: str = None
+  sampleDate: datetime = None
+  sampleInteger: int = None
